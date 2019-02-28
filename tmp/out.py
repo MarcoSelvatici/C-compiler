c00009427db2fc1b79297e46964c2269fbfb3f40
Marco Selvatici <marcoselvatici98@gmail.com>
2019-02-28 13:44:15 +0000

    example of translation

--- tmp/out.py
+++ tmp/out.py
@@ -1,10 +1,28 @@
 
-def f(a, b):
-  return (a + b)
+def collatz_recursive(n):
+  if (n == 1):
+    return n
+  if ((n % 2) == 0):
+    return (n + collatz_recursive((n / 2)))
+  else:
+    return (n + collatz_recursive(((n * 3) + 1)))
+
+
+def collatz_iterative(n):
+  res = 1
+  while (n != 1):
+    res = (res + n)
+    if ((n % 2) == 0):
+      n = (n / 2)
+    else:
+      n = ((n * 3) + 1)
+  return res
 
 
 def main():
-  return f(1, 3)
+  tmp = (20 % 3)
+  n = ((5 * 3) + (20 - tmp))
+  return (collatz_recursive(n) == collatz_iterative(n))
 
 
 if __name__ == '__main__':
