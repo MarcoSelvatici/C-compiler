92201b41991983b463210f191d148800b40fcf38
Marco Selvatici <marcoselvatici98@gmail.com>
2019-03-11 13:57:56 +0000

    bug fixes in function calls

---
+++ test_deliverable/test_cases/cdrivers/FUN_CALL_PARAMS_T_driver.c
@@ -0,0 +1,5 @@
+int fun_call_params(int a, int b, int c, int d);
+
+int main() {
+  return fun_call_params(1, 2, 3, 4);
+}
---
+++ test_deliverable/test_cases/cprograms/FUN_CALL_PARAMS_T.c
@@ -0,0 +1,3 @@
+int fun_call_params(int a, int b, int c, int d) {
+  return a + b + c + d;
+}
